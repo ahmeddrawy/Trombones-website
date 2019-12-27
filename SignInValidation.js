9dbc4090b0cd02459d422d8f2a16cd24301da3cb
farah afifi <36455840+farah-afifi@users.noreply.github.com>
2019-12-28 01:59:35 +0200

    done merging signup with ui

---
+++ SignInValidation.js
@@ -0,0 +1,26 @@
+function validate()
+{
+	var password = document.getElementById("password").value;
+	var email = document.getElementById("username").value;
+		
+ 	var illegalChars = /\S+@\S+\.\S+/; // allow letters, numbers, and underscores
+
+    if( email == "") {
+        alert( "Please provide your Email!" );
+        return false;
+     }
+    if( password == "" ) {
+        alert( "Please provide your Password!" );
+        return false;
+    }
+
+	if(illegalChars.test(email)){
+		alert("The email contains illegal characters.\n");
+        return false;
+	}
+	else
+	{
+		return true;
+	}
+
+}

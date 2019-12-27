fc144649dbb6b32f0f984b505a96b326a8c2bc87
Nada_Nasser <36134746+Nada-Nasser@users.noreply.github.com>
2019-12-28 00:45:47 +0200

    update all Buttons Shape

--- signup.php
+++ signup.php
@@ -1,20 +1,74 @@
 <html>
 <head>
 
+<link rel="stylesheet" type="text/css" href="editUserPageStyle.css"/>
 
 </head>
 <body>
 
+<header>
+	<img src="icon.png" id="PageIcon"/>
+	<h1 id="PageTitle">Original Trombones</h1>
+	<ul id="PageTags"> 
+		<li><input type="button" class="userButton" value="Back To Profile" onclick ="window.location.href = 'userPage.php'"/></li>
+		<li><input type="button" class="userButton" value="Log Out" onclick="window.location.href = 'index.php'"/> </li>
+	</ul> 
+</header>
+
+<div id="content">
+	<form action="signup.php" method="post" name="signUpForm" onsubmit="return(validate());">	
+		<table border="0">
+			<tr> 
+				<td colspan="2" id="tabletitle" > Create a New Account to be able to buy from us </td> 
+			</tr>
+			
+			<tr> 
+				<td id = "infotxt">Name :</td>
+				<td id = "formitems"> <input id = "inputTxt" type ="text" name ="username"/></td>
+			</tr>
+			
+			<tr> 
+				<td id = "infotxt">Email :</td>
+				<td id = "formitems"> <input id = "inputTxt"  name ="email" type="e-mail"/></td>
+			</tr>
+			
+			<tr> 
+				<td id = "infotxt">Age :</td>
+				<td id = "formitems"><input id = "inputTxt" name="age" type="number"/></td>
+			</tr>
+			<tr> 
+				<td id = "infotxt">Password :</td>
+				<td id = "formitems"><input id = "inputTxt" name="password" type="Password"/></td>
+			</tr>
+			
+			<tr> 
+				<td id = "infotxt">confirm Password :</td>
+				<td id = "formitems"> <input id = "inputTxt"  name ="password2" type="Password"/></td>
+			</tr>
+			
+			
+			<tr>
+				<th colspan="2"> <input  type="submit" value="sign up" id ="updateBtn"/><th>
+			</tr>
+		</table>
+	</form>
+	
+</div>
+	
 
 <!-- validate username & password inputs using (js and/or php) and go to userPaga.html -->
 <!-- insert the new user in database -->
-<form name="signupForm">
-username : <input type="text" id="username" /> <br/>
-password : <input type="password" id="password"/> <br/>
-email 	 : <input type="email" id="email"/> <br/>
-telephone : <input type = "text" id="telephone" /> <br/>
-<input type="submit" value="sign up" id = "signupBtn"/>
-</form>
+<!--
+<form action="signup.php" method="post" name="signUpForm" onsubmit="return(validate());" id="signUpForm">
 
+username : <input type ="text" name ="username" id="username"/> <br/> <br/>
+email : <input type="email" name ="email" id="email"/> <br/> <br/>
+password : <input type="password" name="password" id="password"/> <br/> <br/>
+confirm Password : <input type="password" name="password2" id="password2"/> <br/> <br/>
+
+<input name="add" value="add" type="submit"/>
+
+</form>
+-->
 </body>
 </html>

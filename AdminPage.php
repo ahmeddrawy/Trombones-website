60b3ae8c33d790e0a0e0263710199a28d123c147
Nada_Nasser <36134746+Nada-Nasser@users.noreply.github.com>
2019-12-28 12:49:54 +0200

    Update AdminPage.php

--- AdminPage.php
+++ AdminPage.php
@@ -5,6 +5,7 @@
 </head>
 <body>
 
+
 <form name="addProduct" id="addProduct">
 Brand Name 
 <br/><input type="radio" name="brandName" value="Yamaha"> Yamaha<br>
@@ -14,6 +15,7 @@ Brand Name
 number of new Trombones : <input type="number" name="nNewTrombones" id="number" value="0"/>
 <input type="submit" value="submit"/>
 </form>
+
 
 <form name="EditPrice" id="EditPrice">
 Brand Name 
@@ -25,6 +27,7 @@ New Price <input type="number" name="nTrombones" id="number" value="0"/>
 <input type="submit" value="submit"/>
 </form>
 
+
 <form name="DeleteTrombones" id="DeleteTrombones">
 Brand Name 
 <br/><input type="radio" name="brandName" value="Yamaha"> Yamaha<br>
@@ -34,7 +37,9 @@ Brand Name
 number of new Trombones : <input type="number" name="nDeletedTrombones" id="number" value="0"/>
 <input type="submit" value="submit"/>
 </form>
-<form>
+
+
+<form name="ShowProducts">
 Brand Name 
 <br/><input type="radio" name="brandName" value="Yamaha"> Yamaha<br>
 <input type="radio" name="brandName" value="Mendini"> Mendini<br>
@@ -43,9 +48,7 @@ Brand Name
 <input type="submit" value="submit"/>
 </form>
 
-<form name="ShowProducts">
 
-</form>
 
 </body>
 </html>

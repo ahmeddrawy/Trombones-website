0ce7426bba26ff3b7e63269fa4df7145e72cb963
Nada_Nasser <36134746+Nada-Nasser@users.noreply.github.com>
2019-12-28 12:35:25 +0200

    Update AdminPage.php

--- AdminPage.php
+++ AdminPage.php
@@ -1,1 +1,51 @@
-hello
+<html>
+<head>
+
+
+</head>
+<body>
+
+<form name="addProduct" id="addProduct">
+Brand Name 
+<br/><input type="radio" name="brandName" value="Yamaha"> Yamaha<br>
+<input type="radio" name="brandName" value="Mendini"> Mendini<br>
+<input type="radio" name="brandName" value="Bach"> Bach<br>
+
+number of new Trombones : <input type="number" name="nNewTrombones" id="number" value="0"/>
+<input type="submit" value="submit"/>
+</form>
+
+<form name="EditPrice" id="EditPrice">
+Brand Name 
+<br/><input type="radio" name="brandName" value="Yamaha"> Yamaha<br>
+<input type="radio" name="brandName" value="Mendini"> Mendini<br>
+<input type="radio" name="brandName" value="Bach"> Bach<br>
+
+New Price <input type="number" name="nTrombones" id="number" value="0"/>
+<input type="submit" value="submit"/>
+</form>
+
+<form name="DeleteTrombones" id="DeleteTrombones">
+Brand Name 
+<br/><input type="radio" name="brandName" value="Yamaha"> Yamaha<br>
+<input type="radio" name="brandName" value="Mendini"> Mendini<br>
+<input type="radio" name="brandName" value="Bach"> Bach<br>
+
+number of new Trombones : <input type="number" name="nDeletedTrombones" id="number" value="0"/>
+<input type="submit" value="submit"/>
+</form>
+<form>
+Brand Name 
+<br/><input type="radio" name="brandName" value="Yamaha"> Yamaha<br>
+<input type="radio" name="brandName" value="Mendini"> Mendini<br>
+<input type="radio" name="brandName" value="Bach"> Bach<br>
+
+<input type="submit" value="submit"/>
+</form>
+
+<form name="ShowProducts">
+
+</form>
+
+</body>
+</html>
